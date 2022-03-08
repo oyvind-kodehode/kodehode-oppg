8b0f1f93ed154763c9d14ac0d72fb9d7cacd8563
Øyvind <oyvind.kodehode@gmail.com>
2022-03-08 15:01:36 +0100

    t1: Move count() to t1.js, works with Node.js, HTML is unfinished
    83e4aee6-9ee8-11ec-8b2b-dba55d393beb

---
+++ javascript/t1.js
@@ -0,0 +1,29 @@
+/*
+ * t1.js
+ * File ID: 6ca71966-9ee4-11ec-861b-73dad8051f7d
+ */
+
+function count(a) {
+	var i;
+	var text = "";
+
+	for (i = 0; i < a; i++) {
+		text += " " + i;
+
+		if (typeof module !== 'undefined'
+		    && module.exports) {
+			// in Node.js
+			console.log(text);
+		} else {
+			// not in Node.js
+			document.getElementById("skrift").innerHTML = text;
+		}
+	}
+}
+
+if (typeof module !== 'undefined' && module.exports) {
+	// in Node.js
+	count(10);
+}
+
+// vim: set noet ts=8 sw=8 sts=8 :
